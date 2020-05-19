902d17435d4c8bb29c0043d0a0a70ae02e78416f
hoesein <a.heinsoe.a@gmail.com>
2020-05-20 03:36:31 +0630

    Updates

---
+++ precache-manifest.62ad09f7ad6f206cfe1104c5026397a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36fcada4f77c2a372262082e8f754fb",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "27de3f8650721837d7d7",
+    "url": "/COVID-19-Tracker/static/css/main.bf25f75e.chunk.css"
+  },
+  {
+    "revision": "44deb43a9e89391e4538",
+    "url": "/COVID-19-Tracker/static/js/2.8ff86625.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.8ff86625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27de3f8650721837d7d7",
+    "url": "/COVID-19-Tracker/static/js/main.7ac00807.chunk.js"
+  },
+  {
+    "revision": "0fdb1322930838f7ab37",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.8b0e6ea4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/covid.d7265326.png"
+  }
+]);
